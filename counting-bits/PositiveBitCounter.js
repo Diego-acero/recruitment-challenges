c2eb0dc352a3ed01304d73a3d71b685c748216d1
diego acero <diegoaceroarguelles@gmail.com>
2020-07-19 23:34:43 +0200

    Added Count function for BitCounter

--- counting-bits/PositiveBitCounter.js
+++ counting-bits/PositiveBitCounter.js
@@ -1,5 +1,26 @@
 function Count (input) {
-  throw new Error('Not implemented')
+  if(input < 0)
+  {
+    throw new RangeError('Input is negative number');
+  }
+  let inverseBinaryNumber = reverseBinaryNumber(input);
+  return countBinaries (inverseBinaryNumber);
+}
+
+function reverseBinaryNumber(input) {
+  input.toString(2).split("").reverse();
+}
+
+function countBinaries(inverseBinaryNumber) {
+  let bitsCount = [0];
+  for(let i = 0; i < inverseBinaryNumber.length; i++)
+  {
+    if(inverseBinaryNumber[i] == "1")
+    {
+      bitsCount[0]++;
+      bitsCount.push(i);
+    }
+  }
 }
 
 module.exports = { Count }
